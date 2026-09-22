51d105f5e9acfa26d0806d342c6426227410c3fb
OiAlisson <103596988+OiAlisson@users.noreply.github.com>
2026-09-22 20:39:11 -0300

    Create SecurityConfig.java

---
+++ src/main/java/com/agencia/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.agencia.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/destinos/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/destinos").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/destinos/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/destinos/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+        
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

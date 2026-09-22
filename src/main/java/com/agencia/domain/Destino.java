d6ea12b9e67fb157fe28c896ed1cff7b706504bf
OiAlisson <103596988+OiAlisson@users.noreply.github.com>
2026-09-22 20:34:14 -0300

    Create Destino.java

---
+++ src/main/java/com/agencia/domain/Destino.java
@@ -0,0 +1,25 @@
+package com.agencia.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Destino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String localizacao;
+    private Double preco;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getLocalizacao() { return localizacao; }
+    public void setLocalizacao(String localizacao) { this.localizacao = localizacao; }
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
+}

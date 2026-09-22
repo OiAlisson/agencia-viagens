0742fea01ffb6632f83ea13c28482ef8deed4e33
OiAlisson <103596988+OiAlisson@users.noreply.github.com>
2026-09-22 20:38:27 -0300

    Create DestinoRepository.java

---
+++ src/main/java/com/agencia/repository/DestinoRepository.java
@@ -0,0 +1,7 @@
+package com.agencia.repository;
+
+import com.agencia.domain.Destino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DestinoRepository extends JpaRepository<Destino, Long> {
+}
